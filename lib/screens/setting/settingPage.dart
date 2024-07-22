263d60907f55cb98fb571e8aa8c5fc09e6989678
amin75t <167482540+amin75t-work@users.noreply.github.com>
2024-07-22 11:29:49 +0330

    FIXED:remove unknown pack

--- lib/screens/setting/settingPage.dart
+++ lib/screens/setting/settingPage.dart
@@ -1,10 +1,5 @@
-import 'dart:ffi';
-
-import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
-import 'package:test_copilet/res/colors.dart';
 import 'package:test_copilet/widgets/chart.dart';
 
 class SettingPage extends StatelessWidget {
