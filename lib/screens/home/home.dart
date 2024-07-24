880a3284cf6e471d8ae6b7edf7d0f3bceed37b70
amin75t <167482540+amin75t-work@users.noreply.github.com>
2024-07-24 13:17:30 +0330

    UPDATE:update the radiobtn

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -13,11 +13,11 @@ class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
   @override
   Widget build(BuildContext context) {
-  TextEditingController _controllerSearch = TextEditingController();
+    TextEditingController _controllerSearch = TextEditingController();
     return Scaffold(
         backgroundColor: AppColors.bgScreen,
         body: SingleChildScrollView(
-          child:Padding(
+          child: Padding(
             padding: EdgeInsets.all(20),
             child: Column(
               children: [
@@ -26,35 +26,64 @@ class HomeScreen extends StatelessWidget {
                   children: [
                     Row(
                       children: [
-                        SvgPicture.asset("assets/avatar.svg" ,width: 50, height: 50,),
-                        SizedBox(width: 10,),
+                        SvgPicture.asset(
+                          "assets/avatar.svg",
+                          width: 50,
+                          height: 50,
+                        ),
+                        SizedBox(
+                          width: 10,
+                        ),
                         Column(
                           textDirection: TextDirection.ltr,
                           mainAxisAlignment: MainAxisAlignment.start,
                           children: [
-                            Text("Good morning" ,style: AppTextStyles.hint,),
-                            Text("Alexander",style: AppTextStyles.title1,),
+                            Text(
+                              "Good morning",
+                              style: AppTextStyles.hint,
+                            ),
+                            Text(
+                              "Alexander",
+                              style: AppTextStyles.title1,
+                            ),
                           ],
                         ),
                       ],
                     ),
                     Row(
                       children: [
-                        SvgPicture.asset("assets/notificationIcon.svg" ,width: 25, height: 25,),
-                        SizedBox(width: 5,),
-                        Icon(Icons.notifications_none_outlined,color: AppColors.purpleDark,),
+                        SvgPicture.asset(
+                          "assets/notificationIcon.svg",
+                          width: 25,
+                          height: 25,
+                        ),
+                        SizedBox(
+                          width: 5,
+                        ),
+                        Icon(
+                          Icons.notifications_none_outlined,
+                          color: AppColors.purpleDark,
+                        ),
                       ],
                     )
                   ],
                 ),
-                const SizedBox(height: 30,),
+                const SizedBox(
+                  height: 30,
+                ),
                 // AppTextField(lable: 'search', hint: 'search', controller: _controllerSearch,icon: const Icon(Icons.search),),
                 const Longevity(),
-                Container(padding: EdgeInsets.only(top:20,bottom: 10),child: SvgPicture.asset("assets/addBio.svg"),),
+                Container(
+                  padding: EdgeInsets.only(top: 20, bottom: 10),
+                  child: SvgPicture.asset("assets/addBio.svg"),
+                ),
                 Container(
                   padding: EdgeInsets.only(top: 20),
                   alignment: Alignment.centerLeft,
-                  child: Text("Blood Biomarkers",style: AppTextStyles.title1,),
+                  child: Text(
+                    "Blood Biomarkers",
+                    style: AppTextStyles.title1,
+                  ),
                 ),
                 SizedBox(
                     height: 280,
@@ -62,37 +91,60 @@ class HomeScreen extends StatelessWidget {
                       itemCount: 10,
                       scrollDirection: Axis.horizontal,
                       shrinkWrap: true,
-                      padding: const EdgeInsets.only(top: 20 ,bottom: 20,left: 8,right: 10),
+                      padding: const EdgeInsets.only(
+                          top: 20, bottom: 20, left: 8, right: 10),
                       itemBuilder: (BuildContext context, int index) {
                         return ItemCard(
                             title: "Heart Rate",
                             average: "84",
                             icon: SvgPicture.asset(
                               "assets/heart.svg",
-                              colorFilter: ColorFilter.mode(Colors.blue, BlendMode.srcIn),
+                              colorFilter: ColorFilter.mode(
+                                  Colors.blue, BlendMode.srcIn),
                               width: 40,
                               height: 40,
                             ),
                             status: "hi",
                             current: "81");
-                      }, separatorBuilder: (BuildContext context, int index) {
-                      return SizedBox(width: 10,);
-                    },
+                      },
+                      separatorBuilder: (BuildContext context, int index) {
+                        return SizedBox(
+                          width: 10,
+                        );
+                      },
                     )),
                 Center(
                   child: Column(
                     children: [
-                      Text("Check-in to track your progress",style: AppTextStyles.title1,),
+                      Text(
+                        "Check-in to track your progress",
+                        style: AppTextStyles.title1,
+                      ),
                       const Row()
                     ],
                   ),
                 ),
-                const SizedBox(height: 30,),
-                const RadioButton(),
+                const SizedBox(
+                  height: 30,
+                ),
+                Container(
+                  child: Column(
+                    children: [
+                      RadioButton(picUrl: 'assets/yoga.svg', text: 'Try a new yoga flow'),
+                     const SizedBox(height: 10,),
+                      RadioButton(picUrl: 'assets/pill.svg', text: 'Start taking an LAL supplement',),
+                     const SizedBox(height: 10,),
+                      RadioButton(picUrl: 'assets/drag.svg', text: 'Eat a probiotic food today',),
+                     const SizedBox(height: 10,),
+                      RadioButton(picUrl: 'assets/bump.svg', text: 'Bump up your fibre intake',),
+                     const SizedBox(height: 10,),
+
+                    ],
+                  ),
+                )
               ],
             ),
           ),
-        )
-    );
+        ));
   }
 }
