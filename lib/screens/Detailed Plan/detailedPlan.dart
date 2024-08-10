b2d586103a4a308b2c68bcefcba8a10ab9c734cb
amin75t <167482540+amin75t-work@users.noreply.github.com>
2024-08-10 16:57:17 +0330

    FIXED: fixed redirect screen problem

--- lib/screens/Detailed Plan/detailedPlan.dart
+++ lib/screens/Detailed Plan/detailedPlan.dart
@@ -30,42 +30,47 @@ class detailedPlan extends StatelessWidget {
   ];
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      padding: const EdgeInsets.all(16.0),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Image.asset("assets/gymBig.png"),
-          const SizedBox(height: 16.0),
-          const Text(
-            'Level up your aerobic workouts',
-            style: TextStyle(
-              fontSize: 24.0,
-              fontWeight: FontWeight.bold,
-            ),
+    return Scaffold(
+      backgroundColor: AppColors.mainBg,
+      body: SafeArea(
+        child: SingleChildScrollView(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Image.asset("assets/gymBig.png"),
+              const SizedBox(height: 16.0),
+              const Text(
+                'Level up your aerobic workouts',
+                style: TextStyle(
+                  fontSize: 24.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(height: 8.0),
+              Text(
+                '5 days per week, 30 minutes minimum',
+                style: TextStyle(
+                  fontSize: 16.0,
+                  color: Colors.grey[700],
+                ),
+              ),
+              SizedBox(height: 16.0),
+              Text(
+                'Enhance the effectiveness, intensity, or overall quality of your aerobic exercise routines. Aerobic workouts, also known as cardiovascular exercises, are activities that increase your heart rate and improve the efficiency of your cardiovascular system.',
+                style: TextStyle(
+                  fontSize: 16.0,
+                  color: Colors.grey[800],
+                ),
+              ),
+              SizedBox(height: 24.0),
+              CheckInWidget(),
+              DynamicAccordion(
+                sections: sections,
+              )
+            ],
           ),
-          SizedBox(height: 8.0),
-          Text(
-            '5 days per week, 30 minutes minimum',
-            style: TextStyle(
-              fontSize: 16.0,
-              color: Colors.grey[700],
-            ),
-          ),
-          SizedBox(height: 16.0),
-          Text(
-            'Enhance the effectiveness, intensity, or overall quality of your aerobic exercise routines. Aerobic workouts, also known as cardiovascular exercises, are activities that increase your heart rate and improve the efficiency of your cardiovascular system.',
-            style: TextStyle(
-              fontSize: 16.0,
-              color: Colors.grey[800],
-            ),
-          ),
-          SizedBox(height: 24.0),
-          CheckInWidget(),
-          DynamicAccordion(
-            sections: sections,
-          )
-        ],
+        ),
       ),
     );
   }
