c7b916b663d67b6a98a2d3552bc4dd2e79effb4f
amin75t <167482540+amin75t-work@users.noreply.github.com>
2024-10-02 15:12:43 +0330

    UPDATE:make it automatic request in background

--- lib/screens/mainScreenV2/childComponents/permishenHandlerHealth.dart
+++ lib/screens/mainScreenV2/childComponents/permishenHandlerHealth.dart
@@ -1,3 +1,5 @@
+import 'dart:ffi';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
@@ -20,8 +22,26 @@ class Permishenhandlerhealth extends StatefulWidget {
 }
 
 class _PermishenhandlerhealthState extends State<Permishenhandlerhealth> {
+  void _showSnackbar(BuildContext context, String message) {
+    ScaffoldMessenger.of(context)
+        .showSnackBar(SnackBar(content: Text(message)));
+  }
 
-  void initialize(BuildContext context) {
+  void _launchURL(String url) async {
+    if (await launch(url)) {
+      await launch(url);
+    } else {
+      throw '$url';
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    initialize(context);
+  }
+
+  void initialize(BuildContext context)async {
     const environment =
         kDebugMode ? RookEnvironment.sandbox : RookEnvironment.production;
 
@@ -37,18 +57,34 @@ class _PermishenhandlerhealthState extends State<Permishenhandlerhealth> {
     }
 
     HCRookConfigurationManager.setConfiguration(rookConfiguration);
+    var checkVisibleAccount = checkUserIDRegistered();
+    if (checkVisibleAccount) {
+      HCRookConfigurationManager.initRook().then((_) {
+        _showSnackbar(context, 'Rook initialized successfully');
+        updateUserID(context, "amir12@gmail.com");
+        var permissionAndroid = checkAndroidPermissions();
+        if (!permissionAndroid) requestAndroidPermissions(context);
+        if (checkAvailability(context) == HCAvailabilityStatus.installed) {
+          checkHealthConnectPermissions(context);
+          attemptToEnableYesterdaySync(context);
+        }
+      }).catchError((exception) {
+        _showSnackbar(context, 'Error during Rook initialization: $exception');
+      });
+    } else {
+      var permissionAndroid = checkAndroidPermissions();
+      if (!permissionAndroid) requestAndroidPermissions(context);
+      var checkAvailabilityHealthApp = await checkAvailability(context);
+      print("checkAvailabilityHealthApp: $checkAvailabilityHealthApp");
 
-    HCRookConfigurationManager.initRook().then((_) {
-      _showSnackbar(context, 'Rook initialized successfully');
-      updateUserID(context, "amir12@gmail.com");
-      checkAvailability(context);
-      checkHealthConnectPermissions(context);
-
-    }).catchError((exception) {
-      _showSnackbar(context, 'Error during Rook initialization: $exception');
-    });
+      if (checkAvailabilityHealthApp == HCAvailabilityStatus.installed) {
+        checkHealthConnectPermissions(context);
+        attemptToEnableYesterdaySync(context);
+      }
+    }
   }
 
+  // if user not register
   void updateUserID(BuildContext context, String userID) {
     HCRookConfigurationManager.updateUserID(userID).then((_) {
       _showSnackbar(context, 'UserID updated successfully');
@@ -57,32 +93,7 @@ class _PermishenhandlerhealthState extends State<Permishenhandlerhealth> {
     });
   }
 
-  void checkAvailability(BuildContext context) {
-    HCRookHealthPermissionsManager.checkHealthConnectAvailability()
-        .then((availability) {
-      _showSnackbar(context, 'Health Connect available: $availability');
-    }).catchError((exception) {
-      _showSnackbar(
-          context, 'Error checking Health Connect availability: $exception');
-    });
-  }
-
-  void checkHealthConnectPermissions(BuildContext context) {
-    HCRookHealthPermissionsManager.checkHealthConnectPermissions()
-        .then((permissionsGranted) {
-      _showSnackbar(context, 'Permissions granted: $permissionsGranted');
-      if(!permissionsGranted){
-        requestHealthConnectPermissions(context);
-      }
-      else{
-        attemptToEnableYesterdaySync(context);
-      }
-    }).catchError((exception) {
-      _showSnackbar(
-          context, 'Error checking Health Connect permissions: $exception');
-    });
-  }
-
+  //
   void requestHealthConnectPermissions(BuildContext context) {
     HCRookHealthPermissionsManager.requestHealthConnectPermissions()
         .then((requestPermissionsStatus) {
@@ -97,19 +108,7 @@ class _PermishenhandlerhealthState extends State<Permishenhandlerhealth> {
     });
   }
 
-  void _showSnackbar(BuildContext context, String message) {
-    ScaffoldMessenger.of(context)
-        .showSnackBar(SnackBar(content: Text(message)));
-  }
-
-  void _launchURL(String url) async {
-    if (await launch(url)) {
-      await launch(url);
-    } else {
-      throw '$url';
-    }
-  }
-
+  // fetch the health data
   void attemptToEnableYesterdaySync(BuildContext context) {
     SharedPreferences.getInstance().then((prefs) {
       final userAcceptedYesterdaySync =
@@ -117,17 +116,15 @@ class _PermishenhandlerhealthState extends State<Permishenhandlerhealth> {
       const environment =
           kDebugMode ? RookEnvironment.sandbox : RookEnvironment.production;
 
-      requestAndroidPermissions(context);
-
       // Create the configuration for Rook
       if (true) {
-        _showSnackbar(context, "Yesterday sync enabled");
         HCRookYesterdaySyncManager.scheduleYesterdaySync(
           enableNativeLogs: true,
           clientUUID: "b0eb1473-44ed-4c93-8d90-eb15deb20bb7",
           secretKey: "FFybi3eZefYV8ZMhLOeAuT8724oO3ybMkgdR",
           environment: environment,
         );
+        _showSnackbar(context, "Yesterday sync do");
       } else {
         _showSnackbar(context, "User has not accepted yesterday sync");
         // The user did not accept the yesterday sync feature
@@ -137,6 +134,7 @@ class _PermishenhandlerhealthState extends State<Permishenhandlerhealth> {
     });
   }
 
+  // if background run is not permit
   void requestAndroidPermissions(BuildContext context) async {
     try {
       final requestPermissionsStatus =
@@ -152,19 +150,92 @@ class _PermishenhandlerhealthState extends State<Permishenhandlerhealth> {
     }
   }
 
+  // check the connect the app to health app or not
+  void checkHealthConnectPermissions(BuildContext context) {
+    HCRookHealthPermissionsManager.checkHealthConnectPermissions()
+        .then((permissionsGranted) {
+      _showSnackbar(context, 'Permissions granted: $permissionsGranted');
+      requestHealthConnectPermissions(context);
+      if (!permissionsGranted) {
+        requestHealthConnectPermissions(context);
+      }
+    }).catchError((exception) {
+      _showSnackbar(
+          context, 'Error checking Health Connect permissions: $exception');
+    });
+  }
+
+  // check the health app is install or not
+  Future<HCAvailabilityStatus> checkAvailability(BuildContext context) async {
+    try {
+      HCAvailabilityStatus availability = await HCRookHealthPermissionsManager.checkHealthConnectAvailability();
+
+      if (availability == HCAvailabilityStatus.notInstalled) {
+        _launchURL("https://google.com");
+      } else if (availability == HCAvailabilityStatus.notSupported) {
+        _showSnackbar(context, "Device does not support Health Connect.");
+      }
+
+      print("checkAvailabilityHealthApp status: $availability");
+      return availability;
+    } catch (exception) {
+      _showSnackbar(context, 'Error checking Health Connect availability: $exception');
+      return HCAvailabilityStatus.notInstalled;  // Return a default status in case of an error
+    }
+  }
+
+
+  // check the accesses the data background
+  bool checkAndroidPermissions() {
+    bool check=false;
+    HCRookHealthPermissionsManager.checkAndroidPermissions()
+        .then((permissionsGranted) {
+      check = permissionsGranted;
+    }).catchError((exception) {
+      _showSnackbar(context, "error in Permissions android $exception ");
+    });
+    return check;
+  }
+
+  // check user exist
+  bool checkUserIDRegistered() {
+    HCRookConfigurationManager.getUserID().then((userID) {
+      if (userID != null) {
+        print(userID);
+        return true;
+      } else {
+        print("user not found");
+        return false;
+      }
+    });
+    return false;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: GestureDetector(
-        onTap: () {
-          if (false) {
-            _launchURL(
-                'https://connections.rook-connect.review/client_uuid/d2c34b45-51ff-4ef0-95dc-d87c39136469/user_id/aUniqueUserIdABCD1234/');
-          } else {
-            initialize(context);
-          }
-        },
-        child: const Text("init the device"),
+      child: Column(
+        children: [
+          // GestureDetector(
+          //   onTap: () {
+          //     if (false) {
+          //       _launchURL(
+          //           'https://connections.rook-connect.review/client_uuid/d2c34b45-51ff-4ef0-95dc-d87c39136469/user_id/aUniqueUserIdABCD1234/');
+          //     } else {
+          //       initialize(context);
+          //     }
+          //   },
+          //   child: const Text("init the device"),
+          // ),
+          // GestureDetector(
+          //   onTap: checkUserIDRegistered,
+          //   child: Text("checkUserIDRegistered"),
+          // ),
+          // GestureDetector(
+          //   onTap: () => attemptToEnableYesterdaySync(context),
+          //   child: Text("attemptToEnableYesterdaySync"),
+          // )
+        ],
       ),
     );
   }
