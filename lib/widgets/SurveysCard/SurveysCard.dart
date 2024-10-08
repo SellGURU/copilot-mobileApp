3d0f6efa5e922f9118f628ea360aaa29854a7861
amin75t <167482540+amin75t-work@users.noreply.github.com>
2024-10-09 00:37:33 +0330

    UPDATE:update ui

--- lib/widgets/SurveysCard/SurveysCard.dart
+++ lib/widgets/SurveysCard/SurveysCard.dart
@@ -1,4 +1,5 @@
 import 'package:copilet/components/text_style.dart';
+import 'package:copilet/res/colors.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
@@ -28,8 +29,7 @@ class _SurveyCardState extends State<SurveyCard> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      margin: const EdgeInsets.only(bottom: 10),
-      padding: EdgeInsets.symmetric(vertical: 16.0, horizontal: 10),
+
       decoration: BoxDecoration(
         color: Colors.white,
         borderRadius: BorderRadius.circular(20.0),
@@ -42,68 +42,77 @@ class _SurveyCardState extends State<SurveyCard> {
           ),
         ],
       ),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      child: Stack(
         children: [
-          Row(
+          Positioned(right: 0,child:Image.asset("assets/bg_survey_card.png"),),
+        Container(
+          margin: const EdgeInsets.only(bottom: 10),
+          padding: EdgeInsets.symmetric(vertical: 16.0, horizontal: 10),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              // Icon and Text
               Row(
                 children: [
-                  // SvgPicture.asset("assets/Blood-Drops-Positive--Streamline-Ultimate.svg",width: 30,height: 30,),
-                  Image.asset(widget.imagePath,
-                      width: 35, height: 35, fit: BoxFit.fitWidth),
-                  const SizedBox(width: 10),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
+                  // Icon and Text
+                  Row(
                     children: [
-                      Text(widget.Title, style: AppTextStyles.hintMedium),
-                      SizedBox(height: 5),
-                      Row(
+                      // SvgPicture.asset("assets/Blood-Drops-Positive--Streamline-Ultimate.svg",width: 30,height: 30,),
+                      Image.asset(widget.imagePath,
+                          width: 35, height: 35, fit: BoxFit.fitWidth),
+                      const SizedBox(width: 10),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
-                          const Icon(
-                            Icons.help_outline,
-                            size: 15,
-                            color: Colors.grey,
+                          Text(widget.Title, style: AppTextStyles.hintMedium),
+                          SizedBox(height: 5),
+                          Row(
+                            children: [
+                              const Icon(
+                                Icons.help_outline,
+                                size: 15,
+                                color: Colors.grey,
+                              ),
+                              const SizedBox(width: 5),
+                              Text("${widget.Questions} Questions",
+                                  style: AppTextStyles.hintSmale),
+                              const SizedBox(width: 20),
+                              const Icon(
+                                Icons.timer,
+                                size: 15,
+                                color: Colors.grey,
+                              ),
+                              const SizedBox(width: 5),
+                              Text("${widget.Minutes} Minutes",
+                                  style: AppTextStyles.hintSmale),
+                            ],
                           ),
-                          const SizedBox(width: 5),
-                          Text("${widget.Questions} Questions",
-                              style: AppTextStyles.hintSmale),
-                          const SizedBox(width: 20),
-                          const Icon(
-                            Icons.timer,
-                            size: 15,
-                            color: Colors.grey,
-                          ),
-                          const SizedBox(width: 5),
-                          Text("${widget.Minutes} Minutes",
-                              style: AppTextStyles.hintSmale),
                         ],
                       ),
                     ],
                   ),
                 ],
               ),
+              // Arrow Icon
+              GestureDetector(
+                onTap: () {
+                  rediractUrl(widget.Link);
+                },
+                child: Container(
+                    padding: const EdgeInsets.all(8.0),
+                    decoration: const BoxDecoration(
+                      color: AppColors.purpleDark,
+                      shape: BoxShape.circle,
+                    ),
+                    child: SvgPicture.asset(
+                      "assets/arrow-down.svg",
+                      width: 15,
+                      height: 15,
+                    )),
+              ),
             ],
           ),
-          // Arrow Icon
-          GestureDetector(
-            onTap: () {
-              rediractUrl(widget.Link);
-            },
-            child: Container(
-                padding: EdgeInsets.all(8.0),
-                decoration: BoxDecoration(
-                  color: Colors.purple[100],
-                  shape: BoxShape.circle,
-                ),
-                child: SvgPicture.asset(
-                  "assets/arrow-down.svg",
-                  width: 15,
-                  height: 15,
-                )),
-          ),
-        ],
+        ),
+      ]
       ),
     );
   }
