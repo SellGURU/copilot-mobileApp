5a35f530903cf0913b94427e6234245a430a8683
amin <aminshahini18@gmail.com>
2024-11-11 16:18:15 +0330

    FIXED:scroll item can be scroll

--- lib/screens/result/result.dart
+++ lib/screens/result/result.dart
@@ -42,6 +42,7 @@ class _ResultScreenState extends State<ResultScreen> {
         print("data:${data}");
         if (state is SuccessBiomarkerState) {
           return SingleChildScrollView(
+
             child: Container(
               margin: EdgeInsets.only(top: size.height * .02),
               padding: const EdgeInsets.symmetric(vertical: 5, horizontal: 20),
@@ -89,175 +90,217 @@ class _ResultScreenState extends State<ResultScreen> {
                                 0, 1), // changes position of shadow
                           ),
                         ]),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        GestureDetector(
-                          onTap: () {
-                            setState(() {
-                              indexItem = 0;
-                              itemSelectedFiltered = "blood";
-                            });
-                          },
-                          child: Container(
-                            decoration: BoxDecoration(
-                              borderRadius:
-                                  BorderRadius.all(Radius.circular(15)),
-                              color: indexItem == 0
-                                  ? AppColors.purpleDark
-                                  : Colors.white,
-                            ),
-                            height: 40,
-                            child: Padding(
-                              padding: const EdgeInsets.symmetric(
-                                  horizontal: 9.0, vertical: 8),
-                              child: Row(
-                                children: [
-                                  SvgPicture.asset("assets/drops.svg",
-                                      colorFilter: ColorFilter.mode(
-                                          indexItem == 0
-                                              ? AppColors.mainBg
-                                              : AppColors.textLite,
-                                          BlendMode.srcIn)),
-                                  const SizedBox(
-                                    width: 7,
-                                  ),
-                                  Text(
-                                    "Cardiovascular...",
-                                    style: indexItem == 0
-                                        ? AppTextStyles.hintWhite
-                                        : AppTextStyles.hint,
-                                  )
-                                ],
+                    child: SingleChildScrollView(
+                      scrollDirection: Axis.horizontal,
+                      child: Row(
+                        children: [
+                          GestureDetector(
+                            onTap: () {
+                              setState(() {
+                                indexItem = 0;
+                                itemSelectedFiltered = "blood";
+                              });
+                            },
+                            child: Container(
+                              decoration: BoxDecoration(
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(15)),
+                                color: indexItem == 0
+                                    ? AppColors.purpleDark
+                                    : Colors.white,
+                              ),
+                              height: 40,
+                              child: Padding(
+                                padding: const EdgeInsets.symmetric(
+                                    horizontal: 9.0, vertical: 8),
+                                child: Row(
+                                  children: [
+                                    SvgPicture.asset("assets/drops.svg",
+                                        colorFilter: ColorFilter.mode(
+                                            indexItem == 0
+                                                ? AppColors.mainBg
+                                                : AppColors.textLite,
+                                            BlendMode.srcIn)),
+                                    const SizedBox(
+                                      width: 7,
+                                    ),
+                                    Text(
+                                      "Cardiovascular...",
+                                      style: indexItem == 0
+                                          ? AppTextStyles.hintWhite
+                                          : AppTextStyles.hint,
+                                    )
+                                  ],
+                                ),
                               ),
                             ),
                           ),
-                        ),
-                        GestureDetector(
-                          onTap: () {
-                            setState(() {
-                              indexItem = 1;
-                              itemSelectedFiltered = "activity";
-                            });
-                          },
-                          child: Container(
-                            decoration: BoxDecoration(
-                              borderRadius:
-                                  BorderRadius.all(Radius.circular(15)),
-                              color: indexItem == 1
-                                  ? AppColors.purpleDark
-                                  : Colors.white,
-                            ),
-                            height: 40,
-                            child: Padding(
-                              padding: const EdgeInsets.symmetric(
-                                  horizontal: 9.0, vertical: 8),
-                              child: Row(
-                                children: [
-                                  SvgPicture.asset("assets/weight.svg",
-                                      colorFilter: ColorFilter.mode(
-                                          indexItem == 1
-                                              ? AppColors.mainBg
-                                              : AppColors.textLite,
-                                          BlendMode.srcIn)),
-                                  const SizedBox(
-                                    width: 7,
-                                  ),
-                                  Text(
-                                    "Organ He...",
-                                    style: indexItem == 1
-                                        ? AppTextStyles.hintWhite
-                                        : AppTextStyles.hint,
-                                  )
-                                ],
+                          GestureDetector(
+                            onTap: () {
+                              setState(() {
+                                indexItem = 1;
+                                itemSelectedFiltered = "activity";
+                              });
+                            },
+                            child: Container(
+                              decoration: BoxDecoration(
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(15)),
+                                color: indexItem == 1
+                                    ? AppColors.purpleDark
+                                    : Colors.white,
+                              ),
+                              height: 40,
+                              child: Padding(
+                                padding: const EdgeInsets.symmetric(
+                                    horizontal: 9.0, vertical: 8),
+                                child: Row(
+                                  children: [
+                                    SvgPicture.asset("assets/weight.svg",
+                                        colorFilter: ColorFilter.mode(
+                                            indexItem == 1
+                                                ? AppColors.mainBg
+                                                : AppColors.textLite,
+                                            BlendMode.srcIn)),
+                                    const SizedBox(
+                                      width: 7,
+                                    ),
+                                    Text(
+                                      "Organ He...",
+                                      style: indexItem == 1
+                                          ? AppTextStyles.hintWhite
+                                          : AppTextStyles.hint,
+                                    )
+                                  ],
+                                ),
                               ),
                             ),
                           ),
-                        ),
-                        GestureDetector(
-                          onTap: () {
-                            setState(() {
-                              indexItem = 2;
-                              itemSelectedFiltered = "dna";
-                            });
-                          },
-                          child: Container(
-                            decoration: BoxDecoration(
-                              borderRadius:
-                                  BorderRadius.all(Radius.circular(15)),
-                              color: indexItem == 2
-                                  ? AppColors.purpleDark
-                                  : Colors.white,
-                            ),
-                            height: 40,
-                            child: Padding(
-                              padding: const EdgeInsets.symmetric(
-                                  horizontal: 9.0, vertical: 8),
-                              child: Row(
-                                children: [
-                                  SvgPicture.asset("assets/dna.svg",
-                                      colorFilter: ColorFilter.mode(
-                                          indexItem == 2
-                                              ? AppColors.mainBg
-                                              : AppColors.textLite,
-                                          BlendMode.srcIn)),
-                                  const SizedBox(
-                                    width: 7,
-                                  ),
-                                  Text(
-                                    "Metabolic...",
-                                    style: indexItem == 2
-                                        ? AppTextStyles.hintWhite
-                                        : AppTextStyles.hint,
-                                  )
-                                ],
+                          GestureDetector(
+                            onTap: () {
+                              setState(() {
+                                indexItem = 2;
+                                itemSelectedFiltered = "dna";
+                              });
+                            },
+                            child: Container(
+                              decoration: BoxDecoration(
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(15)),
+                                color: indexItem == 2
+                                    ? AppColors.purpleDark
+                                    : Colors.white,
+                              ),
+                              height: 40,
+                              child: Padding(
+                                padding: const EdgeInsets.symmetric(
+                                    horizontal: 9.0, vertical: 8),
+                                child: Row(
+                                  children: [
+                                    SvgPicture.asset("assets/dna.svg",
+                                        colorFilter: ColorFilter.mode(
+                                            indexItem == 2
+                                                ? AppColors.mainBg
+                                                : AppColors.textLite,
+                                            BlendMode.srcIn)),
+                                    const SizedBox(
+                                      width: 7,
+                                    ),
+                                    Text(
+                                      "Metabolic...",
+                                      style: indexItem == 2
+                                          ? AppTextStyles.hintWhite
+                                          : AppTextStyles.hint,
+                                    )
+                                  ],
+                                ),
                               ),
                             ),
                           ),
-                        ),
-                        GestureDetector(
-                          onTap: () {
-                            setState(() {
-                              indexItem = 3;
-                              itemSelectedFiltered = "AGING";
-                            });
-                          },
-                          child: Container(
-                            decoration: BoxDecoration(
-                              borderRadius:
-                                  BorderRadius.all(Radius.circular(15)),
-                              color: indexItem == 3
-                                  ? AppColors.purpleDark
-                                  : Colors.white,
-                            ),
-                            height: 40,
-                            child: Padding(
-                              padding: const EdgeInsets.symmetric(
-                                  horizontal: 9.0, vertical: 8),
-                              child: Row(
-                                children: [
-                                  SvgPicture.asset("assets/monitor.svg",
-                                      colorFilter: ColorFilter.mode(
-                                          indexItem == 3
-                                              ? AppColors.mainBg
-                                              : AppColors.textLite,
-                                          BlendMode.srcIn)),
-                                  const SizedBox(
-                                    width: 7,
-                                  ),
-                                  Text(
-                                    "Immune...",
-                                    style: indexItem == 3
-                                        ? AppTextStyles.hintWhite
-                                        : AppTextStyles.hint,
-                                  )
-                                ],
+                          GestureDetector(
+                            onTap: () {
+                              setState(() {
+                                indexItem = 3;
+                                itemSelectedFiltered = "AGING";
+                              });
+                            },
+                            child: Container(
+                              decoration: BoxDecoration(
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(15)),
+                                color: indexItem == 3
+                                    ? AppColors.purpleDark
+                                    : Colors.white,
+                              ),
+                              height: 40,
+                              child: Padding(
+                                padding: const EdgeInsets.symmetric(
+                                    horizontal: 9.0, vertical: 8),
+                                child: Row(
+                                  children: [
+                                    SvgPicture.asset("assets/monitor.svg",
+                                        colorFilter: ColorFilter.mode(
+                                            indexItem == 3
+                                                ? AppColors.mainBg
+                                                : AppColors.textLite,
+                                            BlendMode.srcIn)),
+                                    const SizedBox(
+                                      width: 7,
+                                    ),
+                                    Text(
+                                      "Immune...",
+                                      style: indexItem == 3
+                                          ? AppTextStyles.hintWhite
+                                          : AppTextStyles.hint,
+                                    )
+                                  ],
+                                ),
                               ),
                             ),
                           ),
-                        ),
-
-                      ],
+                          GestureDetector(
+                            onTap: () {
+                              setState(() {
+                                indexItem = 4;
+                                itemSelectedFiltered = "Genetic & MSK";
+                              });
+                            },
+                            child: Container(
+                              decoration: BoxDecoration(
+                                borderRadius:
+                                BorderRadius.all(Radius.circular(15)),
+                                color: indexItem == 3
+                                    ? AppColors.purpleDark
+                                    : Colors.white,
+                              ),
+                              height: 40,
+                              child: Padding(
+                                padding: const EdgeInsets.symmetric(
+                                    horizontal: 9.0, vertical: 8),
+                                child: Row(
+                                  children: [
+                                    SvgPicture.asset("assets/monitor.svg",
+                                        colorFilter: ColorFilter.mode(
+                                            indexItem == 3
+                                                ? AppColors.mainBg
+                                                : AppColors.textLite,
+                                            BlendMode.srcIn)),
+                                    const SizedBox(
+                                      width: 7,
+                                    ),
+                                    Text(
+                                      "Genetic & MSK...",
+                                      style: indexItem == 3
+                                          ? AppTextStyles.hintWhite
+                                          : AppTextStyles.hint,
+                                    )
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
                     ),
                   ),
                   const SizedBox(
