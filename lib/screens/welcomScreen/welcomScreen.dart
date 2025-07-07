bd06555325c8d96b36e9f9f5b4e784dfd763b926
amir <amirdehghani137722@gmail.com>
2025-07-07 17:16:59 +0330

    Refactor Welcome Screen layout for improved UI consistency
    - Added padding to the Welcome Screen for better spacing and alignment.
    - Maintained existing text and button functionality while enhancing the overall structure of the UI components.

--- lib/screens/welcomScreen/welcomScreen.dart
+++ lib/screens/welcomScreen/welcomScreen.dart
@@ -32,64 +32,67 @@ class _WelcomscreenState extends State<Welcomscreen> {
                     fit: BoxFit.cover,
                   ),
                 ),
-                Center(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      // Image.asset("assets/logoIcon.png"),
-                      Text("Clinic Logo",
-                          style: AppTextStyles.title4xlLiteWeight),
-                      Text("Welcome to Our Clinic!",
-                          style: AppTextStyles.title3xlLiteWeight),
-                      const SizedBox(
-                        height: 40,
-                      ),
-                      SizedBox(
-                          width: 350,
-                          child: Text(
-                            "If you received an invitation email, please click the button below.",
-                            textAlign: TextAlign.center,
-                            style: AppTextStyles.whiteTitle1,
-                          )),
-                      const SizedBox(
-                        height: 40,
-                      ),
-                      GestureDetector(
-                        onTap: () {
-                          Navigator.pushReplacement(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => const RegisterPage()),
-                          );
-                        },
-                        child: Container(
-                          alignment: Alignment.center,
-                          padding: const EdgeInsets.only(top: 10, bottom: 10),
-                          width: 350,
-                          // margin:
-                          //     EdgeInsets.symmetric(horizontal: size.width / 10),
-                          decoration: BoxDecoration(
-                              color: AppColors.purpleDark,
-                              borderRadius: BorderRadius.circular(20)),
-                          // width: size.width,
-                          child: Text(
-                            "Continue",
-                            style: AppTextStyles.hintWhite,
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 16),
+                  child: Center(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        // Image.asset("assets/logoIcon.png"),
+                        Text("Clinic Logo",
+                            style: AppTextStyles.title4xlLiteWeight),
+                        Text("Welcome to Our Clinic!",
+                            style: AppTextStyles.title3xlLiteWeight),
+                        const SizedBox(
+                          height: 40,
+                        ),
+                        SizedBox(
+                            width: 350,
+                            child: Text(
+                              "If you received an invitation email, please click the button below.",
+                              textAlign: TextAlign.center,
+                              style: AppTextStyles.whiteTitle1,
+                            )),
+                        const SizedBox(
+                          height: 40,
+                        ),
+                        GestureDetector(
+                          onTap: () {
+                            Navigator.pushReplacement(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => const RegisterPage()),
+                            );
+                          },
+                          child: Container(
+                            alignment: Alignment.center,
+                            padding: const EdgeInsets.only(top: 10, bottom: 10),
+                            width: 350,
+                            // margin:
+                            //     EdgeInsets.symmetric(horizontal: size.width / 10),
+                            decoration: BoxDecoration(
+                                color: AppColors.purpleDark,
+                                borderRadius: BorderRadius.circular(20)),
+                            // width: size.width,
+                            child: Text(
+                              "Continue",
+                              style: AppTextStyles.hintWhite,
+                            ),
                           ),
                         ),
-                      ),
-                      const SizedBox(
-                        height: 10,
-                      ),
-                      SizedBox(
-                          width: 350,
-                          child: Text(
-                            "Holisticare is an invite-only app. You'll need an invitation email from LPC to create an account.",
-                            textAlign: TextAlign.center,
-                            style: AppTextStyles.hintWhite,
-                          ))
-                    ],
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        SizedBox(
+                            width: 350,
+                            child: Text(
+                              "Holisticare is an invite-only app. You'll need an invitation email from LPC to create an account.",
+                              textAlign: TextAlign.center,
+                              style: AppTextStyles.hintWhite,
+                            ))
+                      ],
+                    ),
                   ),
                 ),
               ],
