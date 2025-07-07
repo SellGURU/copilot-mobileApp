4eabf5f058a5650d61705fb246a24d606c2b0399
MloDehghani <amir.dehghani.official@gmail.com>
2025-07-07 04:47:17 +0000

    update package name

--- android/app/src/main/kotlin/com/example/copilot_mobileapp/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.innovatify_ltd
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
---
+++ android/app/src/main/kotlin/com/innovatifyltd/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.innovatifyltd
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
