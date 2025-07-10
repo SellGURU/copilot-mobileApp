665a82094040a06d1f110dac0a2421c4390d3696
amir <amirdehghani137722@gmail.com>
2025-07-10 21:39:47 +0330

    Update version number and comment out unused UI elements in MainScreenV2 and SettingPage for improved code clarity

--- lib/screens/mainScreenV2/MainScreenV2.dart
+++ lib/screens/mainScreenV2/MainScreenV2.dart
@@ -530,33 +530,34 @@ class _Overview2State extends State<Overview2> {
                                     },
                                     builder: (context, state) {
                                       print("State is: \$state");
-
+                                      
                                       // State: SuccessDownloadPdf
                                       if (state is SuccessDownloadPdf) {
-                                        return GestureDetector(
-                                          onTap: () async {
-                                            // Launch the URL for the PDF download
-                                            LaunchURL(state.pdfUrl);
-                                          },
-                                          child: Row(
-                                            children: [
-                                              SvgPicture.asset(
-                                                "assets/document-download.svg",
-                                                width: 16,
-                                                height: 16,
-                                                colorFilter: const ColorFilter.mode(
-                                                  AppColors.purpleDark,
-                                                  BlendMode.srcIn,
-                                                ),
-                                              ),
-                                              const SizedBox(width: 5),
-                                              Text(
-                                                "Report",
-                                                style: AppTextStyles.hintPurple,
-                                              ),
-                                            ],
-                                          ),
-                                        );
+                                        // return GestureDetector(
+                                        //   onTap: () async {
+                                        //     // Launch the URL for the PDF download
+                                        //     LaunchURL(state.pdfUrl);
+                                        //   },
+                                        //   child: Row(
+                                        //     children: [
+                                        //       SvgPicture.asset(
+                                        //         "assets/document-download.svg",
+                                        //         width: 16,
+                                        //         height: 16,
+                                        //         colorFilter: const ColorFilter.mode(
+                                        //           AppColors.purpleDark,
+                                        //           BlendMode.srcIn,
+                                        //         ),
+                                        //       ),
+                                        //       const SizedBox(width: 5),
+                                        //       Text(
+                                        //         "Report",
+                                        //         style: AppTextStyles.hintPurple,
+                                        //       ),
+                                        //     ],
+                                        //   ),
+                                        // );
+                                         return const SizedBox();
                                       }
 
                                       // State: LoadingDownloadPdf
