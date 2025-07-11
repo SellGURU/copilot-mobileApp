7b46ac4de001987dc1a346ea9596fc5d42131d4f
amir <amirdehghani137722@gmail.com>
2025-07-11 16:57:22 +0330

    Comment out unused permissions in AndroidManifest.xml and remove commented GestureDetector code in bottomNavigationBar.dart for improved code clarity.

--- lib/widgets/bottomNavigationBar.dart
+++ lib/widgets/bottomNavigationBar.dart
@@ -293,52 +293,53 @@ class _ListItemsState extends State<ListItems> {
           //   width: 15,
           // ),
           // itemSelect=3;
-          GestureDetector(
-            onTap: () {
-              setState(() {
-                isCameraStart = true;
-              });
-              Navigator.of(widget.Parentcontext).pushReplacement(
-                MaterialPageRoute<void>(
-                  // provide the Bloc to other screen
-                  builder: (_) => BlocProvider.value(
-                      value: BlocProvider.of<CameraBloc>(widget.Parentcontext),
-                      child: CameraScreen(
-                        isCameraStart: isCameraStart,
-                        Parentcontext: widget.Parentcontext,
-                      )),
-                ),
-              );
-            },
-            child: Container(
-              padding: const EdgeInsets.only(
-                  left: 3.5, right: 3.5, top: 8, bottom: 13),
-              alignment: Alignment.center,
-              width: 90,
-              height: 75,
-              decoration: const BoxDecoration(
-                  color: AppColors.mainBg,
-                  borderRadius: BorderRadius.all(Radius.circular(5))),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  SvgPicture.asset(
-                    "assets/camera.svg",
-                    width: 24,
-                    height: 24,
-                  ),
-                  const SizedBox(
-                    height: 5,
-                  ),
-                  Text(
-                    'Take Picture',
-                    style: AppTextStyles.hint,
-                  ),
-                ],
-              ),
-            ),
-          ),
+          // GestureDetector(
+          //   onTap: () {
+          //     setState(() {
+          //       isCameraStart = true;
+          //     });
+          //     Navigator.of(widget.Parentcontext).pushReplacement(
+          //       MaterialPageRoute<void>(
+          //         // provide the Bloc to other screen
+          //         builder: (_) => BlocProvider.value(
+          //             value: BlocProvider.of<CameraBloc>(widget.Parentcontext),
+          //             child: CameraScreen(
+          //               isCameraStart: isCameraStart,
+          //               Parentcontext: widget.Parentcontext,
+          //             )),
+          //       ),
+          //     );
+          //   },
+          //   child: Container(
+          //     padding: const EdgeInsets.only(
+          //         left: 3.5, right: 3.5, top: 8, bottom: 13),
+          //     alignment: Alignment.center,
+          //     width: 90,
+          //     height: 75,
+          //     decoration: const BoxDecoration(
+          //         color: AppColors.mainBg,
+          //         borderRadius: BorderRadius.all(Radius.circular(5))),
+          //     child: Column(
+          //       mainAxisAlignment: MainAxisAlignment.center,
+          //       crossAxisAlignment: CrossAxisAlignment.center,
+          //       children: [
+          //         SvgPicture.asset(
+          //           "assets/camera.svg",
+          //           width: 24,
+          //           height: 24,
+          //         ),
+          //         const SizedBox(
+          //           height: 5,
+          //         ),
+          //         Text(
+          //           'Take Picture',
+          //           style: AppTextStyles.hint,
+          //         ),
+          //       ],
+          //     ),
+          //   ),
+          // ),
+        
         ],
       ),
     );
