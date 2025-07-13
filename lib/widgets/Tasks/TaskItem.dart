761dfec7ad98284ad5010ee0f2a8b07b6e7d49ea
amir <amirdehghani137722@gmail.com>
2025-07-13 10:07:04 +0330

    Refactor TaskItem widget to use DraggableScrollableSheet for improved user experience
    - Replaced SizedBox with DraggableScrollableSheet to allow for better scrolling behavior.
    - Adjusted height parameters for enhanced flexibility in displaying content.

--- lib/widgets/Tasks/TaskItem.dart
+++ lib/widgets/Tasks/TaskItem.dart
@@ -71,25 +71,32 @@ class _TaskItemState extends State<TaskItem> {
         context: context,
         isScrollControlled: true,
         builder: (context) {
-          return SizedBox(
-            height: MediaQuery.of(context).size.height * 0.9,
-            child: Column(
-              children: [
-                AppBar(
-                  title: Text(title, style: AppTextStyles.title1),
-                  automaticallyImplyLeading: false,
-                  actions: [
-                    IconButton(
-                      icon: const Icon(Icons.close),
-                      onPressed: () => Navigator.pop(context),
+          return DraggableScrollableSheet(
+            expand: false,
+            maxChildSize: 0.95,
+            minChildSize: 0.5,
+            initialChildSize: 0.9,
+            builder: (context, scrollController) {
+              return Column(
+                children: [
+                  AppBar(
+                    title: Text(title, style: AppTextStyles.title1),
+                    automaticallyImplyLeading: false,
+                    actions: [
+                      IconButton(
+                        icon: const Icon(Icons.close),
+                        onPressed: () => Navigator.pop(context),
+                      ),
+                    ],
+                  ),
+                  Expanded(
+                    child: WebViewWidget(
+                      controller: _controller,
                     ),
-                  ],
-                ),
-                Expanded(
-                  child: WebViewWidget(controller: _controller),
-                ),
-              ],
-            ),
+                  ),
+                ],
+              );
+            },
           );
         },
       );
