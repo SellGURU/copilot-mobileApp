7d2e18d753132895f858757dc817c1f9bd5e8215
amir <amirdehghani137722@gmail.com>
2025-07-21 17:44:17 +0330

    Refactor ActivityTaskWrapper and TaskWrapper to handle empty task lists
    - Added a check to return an empty widget when the task list is empty, improving UI responsiveness.
    - Updated the layout structure for better readability and maintainability, including adjustments to the SVG asset used in ActivityTaskWrapper.

--- lib/widgets/Tasks/TaskWrapper.dart
+++ lib/widgets/Tasks/TaskWrapper.dart
@@ -38,38 +38,39 @@ class _TaskWrapperState extends State<TaskWrapper> {
 
   @override
   Widget build(BuildContext context) {
+    if (widget.tasks.isEmpty) {
+      return SizedBox.shrink();
+    }
     return Column(
-              children: [
-                Row(
-                  spacing: 4,
-                  children: [
-                    SvgPicture.asset(
-                        getTypeIconAssetPath(widget.typeName),
-                        fit: BoxFit.cover,
-                        width:18,
-                        height:18
-                    ),
-                    Text(widget.typeName,style: AppTextStyles.hintSmalePrimary,)
-                  ],
-                )
-                ,
-                Container(
-                  margin: const EdgeInsets.only(top: 8),
-                  padding:const EdgeInsets.only(left: 8),
-                  decoration: const BoxDecoration(
-                    border: Border(left: BorderSide(color: AppColors.SilverGray,width: 1,))
-                  ),
-                  child:Column(
-                  children: List.generate(widget.tasks.length,
-                  (index) => 
-                    Padding(padding: const EdgeInsets.only(bottom: 4),
-                    child: TaskItem(task: widget.tasks[index],),
-                    ),
-                  )
-                )
-                ,
+      children: [
+        Row(
+          spacing: 4,
+          children: [
+            SvgPicture.asset(
+                getTypeIconAssetPath(widget.typeName),
+                fit: BoxFit.cover,
+                width:18,
+                height:18
+            ),
+            Text(widget.typeName,style: AppTextStyles.hintSmalePrimary,)
+          ],
+        ),
+        Container(
+          margin: const EdgeInsets.only(top: 8),
+          padding:const EdgeInsets.only(left: 8),
+          decoration: const BoxDecoration(
+            border: Border(left: BorderSide(color: AppColors.SilverGray,width: 1,))
+          ),
+          child:Column(
+            children: List.generate(widget.tasks.length,
+              (index) => 
+                Padding(padding: const EdgeInsets.only(bottom: 4),
+                  child: TaskItem(task: widget.tasks[index],),
                 ),
-              ],
-            );    
+            )
+          )
+        ),
+      ],
+    );    
   }
 }
