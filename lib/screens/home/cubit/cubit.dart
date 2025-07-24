1efb8d7b24e77f02b8a05ff1f2587edf5a3c396a
amir <amirdehghani137722@gmail.com>
2025-07-24 09:36:35 +0330

    update

--- lib/screens/home/cubit/cubit.dart
+++ lib/screens/home/cubit/cubit.dart
@@ -51,10 +51,15 @@ class BiomarkerCubit extends Cubit<BiomarkerState> {
               "data": biomarkers 
             };
             
-            print("Transformed data: ${json.encode(transformedData)}");
-            print("Number of biomarkers: ${(transformedData['data'] as List).length}");
-             print("Transformed data: ${transformedData['data']}");
-            emit(SuccessBiomarkerState(biomarkerData: transformedData));
+            // print("Transformed data: ${json.encode(transformedData)}");
+            // print("Number of biomarkers: ${(transformedData['data'] as List).length}");
+            //  print("Transformed data: ${transformedData['data']}");
+            if(transformedData['data'].isNotEmpty){
+              emit(SuccessBiomarkerState(biomarkerData: transformedData));
+            }else{
+              emit(ErrorBiomarkerState());
+            }
+            // emit(SuccessBiomarkerState(biomarkerData: transformedData));
           } else {
             print("Unexpected response type: ${value.data.runtimeType}");
             emit(ErrorBiomarkerState());
